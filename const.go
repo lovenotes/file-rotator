6446260d63ac37c183b371b4eb1bb58c10f04199
lidonglin <4388193@qq.com>
2019-01-23 12:21:11 +0800

    添加FileRotator代码

---
+++ const.go
@@ -0,0 +1,38 @@
+package filerotator
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+const (
+	_  = iota
+	KB = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+const (
+	LOG_LEVEL_ERROR = 1
+	LOG_LEVEL_WARN  = 2
+	LOG_LEVEL_INFO  = 3
+	LOG_LEVEL_DEBUG = 4
+)
+
+const (
+	LOG_CHECK_INTERVAL = 2 * time.Minute
+	LOG_CHECK_EXPIRED  = 2 * time.Hour
+)
+
+var (
+	_std_error = log.New(os.Stderr, "\033[0;31mERROR:\033[0m ", log.LstdFlags|log.Lshortfile)
+	_std_info  = log.New(os.Stderr, "INFO : ", log.LstdFlags|log.Lshortfile)
+	_std_warn  = log.New(os.Stderr, "\033[0;35mWARN :\033[0m ", log.LstdFlags|log.Lshortfile)
+	_std_raw   = log.New(os.Stderr, "", 0)
+)
